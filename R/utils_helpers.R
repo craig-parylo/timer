ca57a46c84e9f6a09ab3413d1cf2c8e9ee5a9008
Craig Parylo <105867449+craig-parylo@users.noreply.github.com>
2025-08-15 09:53:10 +0100

    renames the file containing general utility functions

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -1,6 +1,5 @@
-
 #' Check whether a supplied datetime is of POSIXct type
-#' 
+#'
 #' @description
 #' If the supplied value inherits from the 'POSIXct' data type then it is
 #' returned, otherwise the function call is aborted and console error raised.
@@ -11,7 +10,7 @@
 #' @examples
 #' # if the parameter is valid POSIXct then it is returned
 #' check_datetime(as.POSIXct("2025-08-12 08:00:00"))
-#' 
+#'
 #' # if the parameter is not a valid POSIXct value then an error is returned
 #' check_datetime("2025-08-12 08:00:00")
 
@@ -25,13 +24,13 @@ check_datetime <- function(datetime_value) {
 }
 
 #' Calculate the duration between two time periods
-#' 
+#'
 #' @description
 #' Calculates the duration between two time periods.
-#' 
+#'
 #' @details
 #' Uses functions from the lubridate package to first calculate the `interval`,
-#' which is defined as 'time spans bound by two real date-times', which are 
+#' which is defined as 'time spans bound by two real date-times', which are
 #' rounded to the nearest second, before being converted to a `period` to
 #' report the elapsed time using the appropriate units, e.g. 1H 18M 0S.
 #'
@@ -47,20 +46,84 @@ check_datetime <- function(datetime_value) {
 #'   .end_time = lubridate::as_datetime("2025-08-13 08:34:00")
 #' )
 calculate_duration <- function(.start_time, .end_time) {
-
   # validate the two parameters as POSIXct datetimes
   .start_time <- check_datetime(.start_time)
   .end_time <- check_datetime(.end_time)
 
   # calculate the time taken as a lubridate period object
-  time_taken <- 
+  time_taken <-
     lubridate::as.period(
       lubridate::interval(
         start = .start_time |> lubridate::round_date(unit = "second"),
         end = .end_time |> lubridate::round_date(unit = "second")
-      ) 
+      )
     )
-  
+
   # return the value
   return(time_taken)
+}
+
+#' Get details for projects ready for use in a select input
+#'
+#' @description
+#' Returns a named list of projects ready for use in a select input
+#'
+#' @details
+#' Gets a tibble of project details from the `get_project_records()` function,
+#' limits the details to project name and id values then creates a named list
+#' suitable for use with a select input control.
+#'
+#' @seealso
+#' [get_project_records()] To return a tibble of project details
+#'
+#' @returns List of named projects with the corresponding database id numbers
+#'
+#' @export
+#' @examples
+#' get_project_choices()
+get_project_choices <- function() {
+  # get all project details as a named list
+  projects_list <-
+    get_project_records() |>
+    # limit to just two columns - id and name
+    dplyr::select(
+      dplyr::all_of(
+        c("name", "id")
+      )
+    ) |>
+    # convert to a named list object
+    tibble::deframe()
+
+  # return this value
+  return(projects_list)
+}
+
+#' Get a list of timescales
+#'
+#' @description
+#' Returns a named list of timescales along with the corresponding id value
+#' for the day or week for use with the 'data view' module
+#'
+#' @details
+#' Returns a named list of time periods:
+#' - 'Today' and 'Yesterday' contain the id values for the corresponding days
+#' - 'This week' and 'Last week' contain the id values for the corresponding weeks
+#' - 'All' is `NA`.
+#'
+#' @returns Named list of time periods and corresponding 'id' values
+#'
+#' @export
+#' @examples
+#' get_timescales_list()
+get_timescales_list <- function() {
+  list_return <- list(
+    "Today" = get_id_date(lubridate::now()),
+    "Yesterday" = get_id_date(lubridate::now() - lubridate::days(1)),
+    "This week" = get_id_week(lubridate::now()),
+    "Last week" = get_id_week(lubridate::now() - lubridate::weeks(1)),
+    "All" = 0
+  )
+
+  # return the list
+  return(list_return)
 }
